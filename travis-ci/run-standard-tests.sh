a95347c8f3322077725921e5a89be11c87d4a5c2
Jakub Sztandera <kubuxu@protonmail.ch>
2017-11-22 11:56:57 +0100

    light cleanup

--- travis-ci/run-standard-tests.sh
+++ travis-ci/run-standard-tests.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 set -e
 
 display_and_run() {
-  echo "*** $@"
+  echo "***" "$@"
   "$@"
 }
 
@@ -30,7 +31,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     # Note: that "go build ./..." will fail if some packages have only
     #   tests (will get "no buildable Go source files" error) so we
     #   have to do this the hard way.
-    go list -f '{{if (len .GoFiles)}}{{.ImportPath}}{{end}}' ./... | grep -v /vendor | \
+    go list -f '{{if (len .GoFiles)}}{{.ImportPath}}{{end}}' ./... | grep -v /vendor/ | \
         while read pkg; do
             (
                 cd "$(mktemp -d)" && display_and_run go build "$pkg"
@@ -38,7 +39,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
         done
     echo
     # Run tests with coverage report in each packages that has tests
-    go list -f '{{if (len .TestGoFiles)}}{{.ImportPath}}{{end}}' ./... | grep -v /vendor > packages-with-tests || true
+    go list -f '{{if (len .TestGoFiles)}}{{.ImportPath}}{{end}}' ./... | grep -v /vendor/ > packages-with-tests || true
     if [[ -s packages-with-tests ]]; then
         cat packages-with-tests | while read pkg; do
             profile="coverage.$(echo "$pkg" | md5sum | head -c 16).txt"
